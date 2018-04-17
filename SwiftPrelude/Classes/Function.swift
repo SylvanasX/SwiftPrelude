7c6a3110c1bd56c36778d6b1d314ad1dec16ad42
SylvanasX <10393625@qq.com>
2018-04-17 14:41:19 +0800

    public func

--- SwiftPrelude/Classes/Function.swift
+++ SwiftPrelude/Classes/Function.swift
@@ -5,10 +5,10 @@
 //  Created by Sylvanas on 2018/4/17.
 //
 
-func |> <A, B>(x: A, f: (A) -> B) -> B {
+public func |> <A, B>(x: A, f: (A) -> B) -> B {
     return f(x)
 }
 
-func >>> <A, B, C> (f: @escaping (A) -> B, g: @escaping (B) -> C) -> (A) -> (C) {
+public func >>> <A, B, C> (f: @escaping (A) -> B, g: @escaping (B) -> C) -> (A) -> (C) {
     return { g(f($0)) }
 }
