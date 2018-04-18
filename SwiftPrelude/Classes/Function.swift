5efb6b080fb4755818d9d3e50b670af0efb5a9e0
SylvanasX <10393625@qq.com>
2018-04-18 21:09:16 +0800

    flip update

--- SwiftPrelude/Classes/Function.swift
+++ SwiftPrelude/Classes/Function.swift
@@ -27,6 +27,10 @@ public func flip <A, B, C> (_ f: @escaping (A) -> (B) -> C) -> (B) -> (A) -> C {
     return { b in { a in f(a)(b) }}
 }
 
+public func flip <A, C> (_ f: @escaping (A) -> () -> C) -> () -> (A) -> C {
+    return { { a in f(a)() }}
+}
+
 public func zurry <A> (_ f: () -> A) -> A {
     return f()
 }
